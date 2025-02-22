3b896eb35684115b7155a05c07b5801cd846e9bd
Giovanni Colabella <giovannicolabell@gmail.com>
2025-02-22 10:33:39 +0100

    Fix navbar menu for mobile users

--- obj/Debug/net8.0/MyCourse_Custom.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyCourse_Custom.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCourse_Custom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891369f0ae1c08834a8191935e8fa1d340908690")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCourse_Custom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCourse_Custom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
